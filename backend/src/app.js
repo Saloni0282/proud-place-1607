e48f07a8293771fc9161f52eec26487c1333283a
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2023-02-24 23:08:04 +0530

    com

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,30 @@ const app = express();
 const hbs  = require('hbs') ;
 // const bodyParser = require('body-parser');
 
-const { routes } = require('./routes/main');
 
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+const { routes } = require('./models/Details');
+const {Detail}=require('./routes/detail');
+//->/static/images/pic.jpg
+app.use("/static",express.static('public'));
 app.use("/", routes)
 
 //(template engine)
 app.set('view engine','hbs')
 app.set('views', "views")
+hbs.registerPartials("views/partials");
 
-
+//db connection
+mongoose.connect('mongodb://localhost/website_tut', () => {
+    console.log('Connected to database');
+    Detail.create({
+      brandName: "Beauty Hub",
+    });
+       
+        
+    
+});
 
 app.listen(process.env.port | 5555, () => {
     console.log('Server is running');
